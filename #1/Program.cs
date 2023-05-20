98c2ae875bf89946e49fa485d8cae4963af9c80c
Gleb Rysev <botkot1991@gmail.com>
2023-05-21 00:34:16 +0400

    add #1

---
+++ #1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+int max = a > b ? a : b;
+
+Console.WriteLine("Большее число: " + max);
+Console.WriteLine("Меньшее число: " + (a + b - max));
