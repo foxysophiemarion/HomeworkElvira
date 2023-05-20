e5dacf987dddb240656d7f2f6c1b2bf3abc89d16
Gleb Rysev <botkot1991@gmail.com>
2023-05-21 00:53:48 +0400

    add #3

---
+++ #3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+	Console.WriteLine("Число " + num + " четное");
+}
+else
+{
+	Console.WriteLine("Число " + num + " нечетное");
+}
