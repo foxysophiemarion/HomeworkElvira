918a7e804e2142797bba62d8f3c3a40440e819af
Gleb Rysev <botkot1991@gmail.com>
2023-05-29 01:17:26 +0400

    add #13

---
+++ #13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int thirdNumber(int arg)
+{
+	int arg1 = arg / 100;
+	if (arg1 >= 1 & arg1 < 10)
+	{
+		int arg0 = arg % 10;
+		Console.WriteLine("Третья цифра заданного числа: " + arg0);
+	}
+	else
+	{
+		Console.WriteLine("Третьей цифры нет");
+	}
+	return arg1;
+}
+
+int result = thirdNumber(number);
