1110bad1a609a69540e002258cdc9209ecf35a21
Gleb Rysev <botkot1991@gmail.com>
2023-05-29 01:24:41 +0400

    add #15

---
+++ #15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите числовое значение дня недели: ");
+int day = int.Parse(Console.ReadLine());
+if (day == 6 || day == 7)
+{
+	Console.WriteLine("Выходной день!");
+}
+if (day >= 8)
+{
+	Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+	Console.WriteLine("Не выходной день.");
+}
