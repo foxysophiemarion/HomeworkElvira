550590610b8a60a47e8512bd81f5fe10de637be1
Gleb Rysev <botkot1991@gmail.com>
2023-05-29 00:15:23 +0400

    add #10

---
+++ #10/Program.cs
@@ -0,0 +1,5 @@
+﻿int random = new Random().Next(100, 1000);
+Console.WriteLine("Сгенерированное число: " + random);
+
+int number = (random / 10) % 10;
+Console.WriteLine("Вторая цифра трехзначного числа: " + number);
