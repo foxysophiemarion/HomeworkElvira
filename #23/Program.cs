d50d21e4418b1858eb5e70e3caa050b5341d83c6
Gleb Rysev <botkot1991@gmail.com>
2023-06-04 01:27:25 +0400

    add #23

---
+++ #23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Таблица кубов чисел от 1 до " + number + ": ");
+for (int i = 1; i <= number; i++)
+{
+	int cube = i * i * i;
+	Console.WriteLine(i + "^3 = " + cube);
+}
