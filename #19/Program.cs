fb697530aa4f4be1dbf5ab77db3d1be5d0d9f1d9
Gleb Rysev <botkot1991@gmail.com>
2023-06-04 01:04:29 +0400

    add #19

---
+++ #19/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 10000 || number > 99999)
+{
+	Console.WriteLine("Некорректное число!");
+	return;
+}
+
+int digit1 = number / 10000;
+int digit2 = (number / 1000) % 10;
+int digit4 = (number / 10) % 10;
+int digit5 = number % 10;
+
+if (digit1 == digit5 && digit2 == digit4)
+{
+	Console.WriteLine("Число " + number + " является палиндромом");
+}
+else
+{
+	Console.WriteLine("Число " + number + " не является палиндромом");
+}
