47e622e1190862b228da4bbfaa69c15e16921b51
Gleb Rysev <botkot1991@gmail.com>
2023-06-12 23:28:47 +0400

    add #25

---
+++ #25/Program.cs
@@ -0,0 +1,35 @@
+﻿int Promt(string message)
+{
+	System.Console.Write(message);
+	string readInput = System.Console.ReadLine();
+	int result = int.Parse(readInput);
+	return result;
+}
+
+int Degree(int digitA, int digitB)
+{
+	int number = 1;
+	for (int i = 0; i < digitB; i++)
+	{
+		number *= digitA;
+	}
+	return number;
+}
+
+bool numberB(int digitB)
+{
+	if (digitB < 0)
+	{
+		System.Console.WriteLine("Число B не должно быть меньше нуля");
+		return false;
+	}
+	return true;
+}
+
+int digitA = Promt("Введите число A: ");
+int digitB = Promt("Введите число B: ");
+
+if (numberB(digitB))
+{
+	System.Console.WriteLine($"Число {digitA} в степени {digitB} равно {Degree(digitA, digitB)}");
+}
