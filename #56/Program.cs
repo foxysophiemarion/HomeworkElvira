73caa92b421432516193a006b93f1a78dd17332b
Gleb Rysev <botkot1991@gmail.com>
2023-06-22 01:29:25 +0400

    added #56

---
+++ #56/Program.cs
@@ -0,0 +1,36 @@
+﻿int[,] matrix = new int[4, 4];
+
+for (int i = 0; i < 4; i++)
+{
+	for (int j = 0; j < 4; j++)
+	{
+		matrix[i, j] = Prompt($"Введите элемент ({i + 1}, {j + 1})");
+	}
+}
+
+int minSum = int.MaxValue;
+int minSumRow = -1;
+
+for (int i = 0; i < 4; i++)
+{
+	int sum = 0;
+	for (int j = 0; j < 4; j++)
+	{
+		sum += matrix[i, j];
+	}
+	if (sum < minSum)
+	{
+		minSum = sum;
+		minSumRow = i + 1;
+	}
+}
+
+Console.WriteLine($"Строка с наименьшей суммой: {minSumRow}");
+
+
+int Prompt(string message)
+{
+	Console.Write($"{message}: ");
+	string input = Console.ReadLine();
+	return int.Parse(input);
+}
