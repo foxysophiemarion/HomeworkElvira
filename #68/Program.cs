f64a35c44d12d01dbb8e04367f732b44a1bf0613
Gleb Rysev <botkot1991@gmail.com>
2023-06-26 01:27:15 +0400

    add #68

---
+++ #68/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите значение m:");
+int m;
+while (!int.TryParse(Console.ReadLine(), out m) || m < 0)
+{
+	Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+}
+Console.WriteLine("Введите значение n:");
+int n;
+while (!int.TryParse(Console.ReadLine(), out n) || n < 0)
+{
+	Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+}
+Console.WriteLine($"A(m,n) = {Ackermann(m, n)}.");
+
+
+int Ackermann(int m, int n)
+{
+	if (m == 0)
+	{
+		return n + 1;
+	}
+	else if (n == 0)
+	{
+		return Ackermann(m - 1, 1);
+	}
+	else
+	{
+		return Ackermann(m - 1, Ackermann(m, n - 1));
+	}
+}
