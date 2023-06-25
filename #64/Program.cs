8edcc2b5a055b2d11fd0fd4ac908d421c1ff7d10
Gleb Rysev <botkot1991@gmail.com>
2023-06-26 00:54:15 +0400

    add #64

---
+++ #64/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение N: ");
+int n;
+while (!int.TryParse(Console.ReadLine(), out n) || n < 1)
+{
+	Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+}
+PrintNumbers(n);
+
+
+void PrintNumbers(int n)
+{
+	if (n == 0)
+	{
+		return;
+	}
+	Console.WriteLine(n);
+	PrintNumbers(n - 1);
+}
