55d40637816cd52b4cee84269c92407720e1bb37
Gleb Rysev <botkot1991@gmail.com>
2023-06-26 01:02:53 +0400

    add #66

---
+++ #66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение M:");
+int m;
+while (!int.TryParse(Console.ReadLine()!, out m) || m < 1)
+{
+	Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+}
+Console.WriteLine("Введите значение N:");
+int n;
+while (!int.TryParse(Console.ReadLine()!, out n) || n < m)
+{
+	Console.WriteLine("Некорректный ввод. Попробуйте еще раз.");
+}
+int sum = 0;
+for (int i = m; i <= n; i++)
+{
+	sum += i;
+}
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {sum}.");
